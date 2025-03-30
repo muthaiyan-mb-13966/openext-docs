bafbb35e1e7deb44dae47c7499bf4e2f7c1cc8ca
Muthaiyan Mani <151814981+muthaiyan-mb-13966@users.noreply.github.com>
2025-03-30 18:13:23 +0530

    opennext config changes

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,25 +1,22 @@
 
-
-
 const buildConfig = {
-    default: {
-      override: {
-        converter: 'node',
-        wrapper: async () => import('@zcatalyst/nextjs-plugin/dist/adapters/wrapper.js'),
-        incrementalCache: async () => import('@zcatalyst/nextjs-plugin/dist/adapters/incremental-cache.js'),
-        tagCache: async () => import('@zcatalyst/nextjs-plugin/dist/adapters/tag-cache.js'),
-        queue:"direct"
-      },
+  default: {
+    override: {
+      converter: 'node',
+      wrapper: async () => (await import('@zcatalyst/nextjs-plugin/dist/adapters/wrapper.js')).default,
+      incrementalCache: async () => (await import('@zcatalyst/nextjs-plugin/dist/adapters/incremental-cache.js')).default,
+      tagCache: async () => (await import('@zcatalyst/nextjs-plugin/dist/adapters/tag-cache.js')).default,
+      queue:"direct"
     },
-    imageOptimization: {
-      loader: async () => import('@zcatalyst/nextjs-plugin/dist/adapters/image-loader.js'),
-      override:{
-        converter: 'node',
-        wrapper: async () => import('@zcatalyst/nextjs-plugin/dist/adapters/image-function-wrapper.js'),
-      }
-    },
-    buildCommand: process.env.ZC_BUILD_COMMAND ?? "npm run build"
-  };
-  
-  export default buildConfig;
-  
+  },
+  imageOptimization: {
+    loader: async () => (await import('@zcatalyst/nextjs-plugin/dist/adapters/image-loader.js')).default,
+    override:{
+      converter: 'node',
+      wrapper: async () => (await import('@zcatalyst/nextjs-plugin/dist/adapters/image-function-wrapper.js')).default,
+    }
+  },
+  buildCommand: process.env.ZC_BUILD_COMMAND ?? "npm run build"
+};
+
+module.exports = buildConfig;
